6203de6ee087ff902aec1a3ccf1375883367793b
Amir P <apourrezaei@gmail.com>
2019-07-30 17:07:34 -0400

    New Project Object

--- portfolio/src/app/portfolio/portfolio.component.ts
+++ portfolio/src/app/portfolio/portfolio.component.ts
@@ -13,9 +13,14 @@ export class PortfolioComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-       this.portfolioLinks = ["https://apg2677.github.io/unit-4-game/", "https://apg2677.github.io/TriviaGame/",
-      "https://apg2677.github.io/Psychic-Game/", "https://apg2677.github.io/GifTastic/",
-      "https://stark-anchorage-35995.herokuapp.com/", "https://guarded-spire-69237.herokuapp.com/",
-      "https://warm-wave-74995.herokuapp.com/"];
+       this.portfolioLinks = [
+         {id: 1, name: 'test1'},
+         {id: 2, name:'test2'}
+       ];
+     
   }
 }
+// this.portfolioLinks = ["https://apg2677.github.io/unit-4-game/", "https://apg2677.github.io/TriviaGame/",
+//       "https://apg2677.github.io/Psychic-Game/", "https://apg2677.github.io/GifTastic/",
+//       "https://stark-anchorage-35995.herokuapp.com/", "https://guarded-spire-69237.herokuapp.com/",
+//       "https://warm-wave-74995.herokuapp.com/"];
