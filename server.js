4190e32e7f303a1d4b6dd58acef92f6c7414d699
Amir P <apourrezaei@gmail.com>
2019-08-20 17:14:21 -0400

    Folder Struct

---
+++ server.js
@@ -0,0 +1,49 @@
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+// const bodyParser = require('body-parser');
+// const morgan = require('morgan');
+// const proxy = require('express-http-proxy');
+// const cors = require('cors');
+// const request = require('request');
+
+const app = express();
+
+// app.use(cors());
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+console.log("Using node server");
+// var Client = require('node-rest-client').Client;
+// var client = new Client();
+
+// app.get('/server_api/api/v1/rivers', function (req, res) {
+//     client.get("http://riverbrain.com/api/v1/rivers", function (data, response) {
+//         res.send(data);
+//     });
+// });
+
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3001';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on ${port}`));
