c6fbe8aa65a81def50da627d81284753884d54c1
Amir P <apourrezaei@gmail.com>
2019-08-21 17:04:59 -0400

    Bill tracker 2

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,31 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-portfolio',
-  templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css']
+  selector: "app-portfolio",
+  templateUrl: "./portfolio.component.html",
+  styleUrls: ["./portfolio.component.css"]
 })
-
 export class PortfolioComponent implements OnInit {
-  portfolioLinks : any[];
+  portfolioLinks: any[];
 
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-       this.portfolioLinks = [
-         {id: 1, name: 'RPG Game', link: "https://apg2677.github.io/unit-4-game/", img:"./assets/images/rpgImage.jpeg"},
-         {id: 2, name:'Trivia', link: "https://apg2677.github.io/TriviaGame", img:"./assets/images/kramerprotrait.jpeg"},
-         {id:3, name: 'Psychic', link: "https://apg2677.github.io/Psychic-Game/", img:"./assets/images/psychic.jpg"},
-         { id:4, name: 'GifTastic', link:"https://apg2677.github.io/GifTastic/", img:"./assets/images/giftastic.jpeg"},
-         { id:5, name: 'Burger', link:"https://stark-anchorage-35995.herokuapp.com/", img:"./assets/images/burger1.png"},
-         { id:6, name: 'Friend Finder', link:"https://guarded-spire-69237.herokuapp.com/", img:"./assets/images/friend1.png"},
-         { id:7, name: 'Bill Tracker', link:"https://warm-wave-74995.herokuapp.com/", img:"./assets/images/bills.png"},
-       ];
-     
+    this.portfolioLinks = [
+      {
+        id: 1,
+        name: "RPG Game",
+        link: "https://apg2677.github.io/unit-4-game/",
+        img: "./assets/images/rpgImage.jpeg"
+      },
+      {
+        id: 2,
+        name: "Trivia",
+        link: "https://apg2677.github.io/TriviaGame",
+        img: "./assets/images/kramerprotrait.jpeg"
+      },
+      {
+        id: 3,
+        name: "Psychic",
+        link: "https://apg2677.github.io/Psychic-Game/",
+        img: "./assets/images/psychic.jpg"
+      },
+      {
+        id: 4,
+        name: "GifTastic",
+        link: "https://apg2677.github.io/GifTastic/",
+        img: "./assets/images/giftastic.jpeg"
+      },
+      {
+        id: 5,
+        name: "Burger",
+        link: "https://stark-anchorage-35995.herokuapp.com/",
+        img: "./assets/images/burger1.png"
+      },
+      {
+        id: 6,
+        name: "Friend Finder",
+        link: "https://guarded-spire-69237.herokuapp.com/",
+        img: "./assets/images/friend1.png"
+      },
+      {
+        id: 7,
+        name: "Bill Tracker",
+        link: "https://bill-tracker2.herokuapp.com",
+        img: "./assets/images/bills.png"
+      }
+    ];
   }
   LoadProject(project) {
-    alert("Load:" + project.name);
+    // alert("Load:" + project.name);
     window.location.href = project.link;
   }
 }
